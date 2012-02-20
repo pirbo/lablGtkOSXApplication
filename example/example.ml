1de31a9a9e3e657e507a067afc3110ebc4544ccf
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2012-02-20 22:00:17 +0100

    All the methods

--- example/example.ml
+++ example/example.ml
@@ -2,8 +2,10 @@
 let () =
   let locale = GtkMain.Main.init () in
   let osx = GOSXApplication.osxapplication () in
-  let window = GWindow.window ~title:"The example" ~width:220 ~height:100 () in
-  let txt = GMisc.label ~text:(osx#executable_path ()) ~packing:window#add () in
+  let window = GWindow.window ~title:"The example" ~width:300 ~height:200 () in
+  let thebox = GPack.vbox ~packing:window#add () in
+  let txt = GMisc.label ~text:(osx#executable_path ()) ~packing:thebox#pack () in
+  let _ = GText.view ~width:100 ~packing:thebox#pack () in
   let () = osx#ready () in
   let _ = osx#connect#ns_application_open_file ~callback:(fun thefile ->  txt#set_text thefile) in
   let _ = osx#connect#ns_application_block_termination ~callback:(fun _ -> print_string "Au revoir") in
--- src/gOSXApplication.ml
+++ src/gOSXApplication.ml
@@ -9,7 +9,21 @@ end
 class osxapplication obj = object
   method connect = new osx_application_signals obj
   method ready () = OSXApplication.ready obj
+  method cleanup () = OSXApplication.cleanup obj
+  method set_use_quartz_accelerators = OSXApplication.set_use_quartz_accelerators obj
+  method use_quartz_accelerators () = OSXApplication.use_quartz_accelerators obj
+  method set_menu_bar = OSXApplication.set_menu_bar obj
+  method sync_menubar () = OSXApplication.sync_menubar obj
+  method set_dock_menu = OSXApplication.set_dock_menu obj
+  method set_dock_icon_pixbuf = OSXApplication.set_dock_icon_pixbuf obj
+  method set_dock_icon_resource = OSXApplication.set_dock_icon_resource obj
+  method attention_request = OSXApplication.attention_request obj
+  method cancel_attention_request = OSXApplication.cancel_attention_request obj
+  method bundle_path () = OSXApplication.get_bundle_path obj
+  method resource_path () = OSXApplication.get_resource_path obj
   method executable_path () = OSXApplication.get_executable_path obj
+  method bundle_id () = OSXApplication.get_bundle_id obj
+  method bundle_info = OSXApplication.get_bundle_info obj
 end
 
 let osxapplication () = new osxapplication (OSXApplication.create [])
